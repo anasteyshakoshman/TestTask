3070bceb0a91f265f286dcd4862344b10ff19122
anasteyshakoshman <anastasiay.andreevnaa@gmail.com>
2019-12-29 18:35:17 +0300

    begin

---
+++ src/components/Object.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="object">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Object',
+  props: {
+    info: {
+      type: Object,
+      required: true
+    },
+    extendInfo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
